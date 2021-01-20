fff8fbfbd3931234e6d494e91e643014b7194107
Menaka Fernando <project.menaka@gmail.com>
2021-01-20 16:55:05 +0100

    untracked files

---
+++ database/migrations/2021_01_20_115625_create_issues_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIssuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issues', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('departmnet');
+            $table->integer('tec_id');
+            $table->string('type');
+            $table->string('desc');
+            $table->date('deadline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issues');
+    }
+}
